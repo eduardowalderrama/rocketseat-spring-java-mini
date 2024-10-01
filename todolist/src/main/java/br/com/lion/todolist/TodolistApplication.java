570367cfdbdff2bf7d15377964aaa2170014a199
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-01 19:38:07 -0300

    funcionalidade de usuario

--- todolist/src/main/java/br/com/lion/todolist/TodolistApplication.java
+++ todolist/src/main/java/br/com/lion/todolist/TodolistApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodolistApplication {
+public class
+
+TodolistApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
---
+++ todolist/src/main/java/br/com/lion/todolist/user/UserController.java
@@ -0,0 +1,36 @@
+package br.com.lion.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    /**
+     * String (texto)
+     * Integer (int) numeros inteiros
+     * Double (double) Numeros 0.000
+     * Float (float) Numeros 0.000
+     * char (A C )
+     * Date (data)
+     * void
+     * @param userModel
+     */
+    /*
+     * Body
+     */
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+
+}
