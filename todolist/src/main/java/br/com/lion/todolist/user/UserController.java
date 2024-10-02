9dcf6f9e0073a638868c1cbcc15695ff126cde3c
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-02 20:22:10 -0300

    repository

--- todolist/src/main/java/br/com/lion/todolist/user/UserController.java
+++ todolist/src/main/java/br/com/lion/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.lion.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,22 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (int) numeros inteiros
-     * Double (double) Numeros 0.000
-     * Float (float) Numeros 0.000
-     * char (A C )
-     * Date (data)
-     * void
-     * @param userModel
-     */
-    /*
-     * Body
-     */
+    @Autowired
+    private IUserRepository userRepository;
+    
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public UserModel create(@RequestBody UserModel userModel) {
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 
 }
