cf2cc47b8bc34042f604badbf6243461ef4e6b7e
eduardowalderrama <walderramaeduardo@gmail.com>
2024-10-04 20:30:58 -0300

    realizando filtro

---
+++ todolist/src/main/java/br/com/lion/todolist/filter/FilterTaskAuth.java
@@ -0,0 +1,21 @@
+package br.com.lion.todolist.filter;
+
+import jakarta.servlet.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class FilterTaskAuth implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        // Executar alguma acao
+
+        System.out.println("Chegou no filtro");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+}
